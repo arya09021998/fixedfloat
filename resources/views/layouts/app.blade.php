f49f1904caae432a125e1a6a4ad16ef188e6e22a
Narek Melqonyan <narek199121@gmail.com>
2023-12-26 22:47:59 +0400

    Add Google Tag Manager to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,23 @@
             @endif
         </script>
     @endif
-
+    <!-- Google Tag Manager -->
+    <script>
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-MSKPF99V');
+    </script>
+    <!-- End Google Tag Manager -->
     @if(request()->routeIs('*.home'))
         <style type="text/css" id="jssnowflakes">
             .main-bg-snow > .snowflake:nth-child(1) {
